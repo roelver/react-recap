f36f93d2f480276cf6d5845c741c9c9668beecd7
Roel Verbunt <roelv@fastmail.com>
2021-08-07 17:30:07 +0200

    Added page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route } from 'react-router-dom';
+
+import AllMeetups from './pages/AllMeetups';
+import Favorites from './pages/Favorites';
+import NewMeetup from './pages/NewMeetup';
+import MainNavigation from './components/layouts/MainNavigation';
+
 function App() {
-  return <div></div>;
+  return <div>
+    <MainNavigation/>
+    <Route path='/new' component={NewMeetup}></Route>
+    <Route path='/favs' component={Favorites}></Route>
+    <Route path='/' component={AllMeetups} exact ></Route>
+    </div>;
 }
 
 export default App;
---
+++ src/components/layouts/MainNavigation.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function MainNavigation() {
+    return <header>
+        <div>React Meetups</div>
+        <nav>
+            <ul>
+                <li><Link to='/'>All Meetups</Link></li>
+                <li><Link to='/new'>New Meetup</Link></li>
+                <li><Link to='/favs'>Favorites</Link></li>
+            </ul>
+        </nav>
+    </header>;
+}
+
+export default MainNavigation;
